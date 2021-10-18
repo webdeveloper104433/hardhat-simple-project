d42efcf952e5495a5d9dd6fb6b12a60ce81574d6
webdeveloper104433 <web.developer104433@gmail.com>
2021-10-18 15:04:15 +0300

    updated deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,13 @@
 require("@nomiclabs/hardhat-waffle");
 
-// Go to https://www.alchemyapi.io, sign up, create
-// a new App in its dashboard, and replace "KEY" with its key
+
+
 const ALCHEMY_API_KEY = "7d7a6bfa48354518a97d961ccde0ad17";
 
-// Replace this private key with your Ropsten account private key
-// To export your private key from Metamask, open Metamask and
-// go to Account Details > Export Private Key
-// Be aware of NEVER putting real Ether into testing accounts
-const ROPSTEN_PRIVATE_KEY = "2dd341d67e89818973c6ddc3b8a2e619c145bf6b95544f3d7d036e7e4bf0bf09";
 
+
+const RINKEBY_PRIVATE_KEY = "2dd341d67e89818973c6ddc3b8a2e619c145bf6b95544f3d7d036e7e4bf0bf09";
+const LOCALHOST_PRIVATE_KEY = "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -29,10 +27,18 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 module.exports = {
   solidity: "0.8.4",
   networks: {
-    ropsten: {
-      url: `https://rinkeby.infura.io/v3/${ALCHEMY_API_KEY}`,
-      accounts: [`0x${ROPSTEN_PRIVATE_KEY}`],
+    hardhat: {
+      chainId: 1337,
     },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${ALCHEMY_API_KEY}`,
+      accounts: [`0x${RINKEBY_PRIVATE_KEY}`],
+      // Token address: 0xEA48459388dF8AaCbD1Fd89E320ffA5eA0AB6DDe
+    },
+    // localhost: {
+    //   url: `http://127.0.0.1:8545/`,
+    //   accounts: [`0x${LOCALHOST_PRIVATE_KEY}`],
+    // }
   },
 
 };
